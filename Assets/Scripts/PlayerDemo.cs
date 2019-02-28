724a1d4e070f48ad4cd89c6bbf24eef14d85954f
unknown <Amanda.Dittami@iam.local>
2019-02-28 16:45:04 -0600

    doing good stuff!

--- Assets/Scripts/PlayerDemo.cs
+++ Assets/Scripts/PlayerDemo.cs
@@ -7,7 +7,7 @@ public class PlayerDemo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("DO REALLY good stuff.!");
     }
 
     // Update is called once per frame
