e31bed38adf8e823149b0562bc76db5a91b2576e
unknown <Amanda.Dittami@iam.local>
2019-02-28 16:46:24 -0600

    really great feature added.

--- Assets/Scripts/PlayerDemo.cs
+++ Assets/Scripts/PlayerDemo.cs
@@ -7,7 +7,7 @@ public class PlayerDemo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Really great feature added!");
     }
 
     // Update is called once per frame
